e1a840c08a3b7e00c178f9b6e1610b15c812cfbe
Harrison <Harrison@sentdex.com>
2017-04-05 08:49:11 -0500

    Create part-1-Getting-Visuals.py
    init commit

---
+++ part-1-Getting-Visuals.py
@@ -0,0 +1,17 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+
+# just so this doesn't go on forever:
+
+def screen_record(): 
+    last_time = time.time()
+    while(True):
+        # 800x600 windowed mode
+        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        print('loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
