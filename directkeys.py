24c5feb03c60e3abe4631858efd163fe2571574d
Harrison <Harrison@sentdex.com>
2017-04-05 08:55:15 -0500

    Update directkeys.py
    Added StackOverflow's licensing.

--- directkeys.py
+++ directkeys.py
@@ -1,3 +1,4 @@
+# This code is licensed under: https://creativecommons.org/licenses/by-sa/3.0/ since it's coming from stackoverflow. 
 # direct inputs
 # source to this solution and code:
 # http://stackoverflow.com/questions/14489013/simulate-python-keypresses-for-controlling-a-game
