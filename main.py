601ed3d2ae977b8844a109c43d5f1fe73fe3b84b
Harrison <Harrison@sentdex.com>
2017-04-14 18:44:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -88,24 +88,27 @@ for i in list(range(4))[::-1]:
     time.sleep(1)
 
 
-last_time = time.time()
-while True:
-    screen = grab_screen(region=(0,40,800,640))
-    print('Frame took {} seconds'.format(time.time()-last_time))
+    
+def main():
     last_time = time.time()
-    new_screen,original_image, m1, m2 = process_img(screen)
-    #cv2.imshow('window', new_screen)
-    cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
-    
-    if m1 < 0 and m2 < 0:
-        right()
-    elif m1 > 0  and m2 > 0:
-        left()
-    else:
-        straight()
-    
-    #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
 
+    while True:
+        screen = grab_screen(region=(0,40,800,640))
+        print('Frame took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        new_screen,original_image, m1, m2 = process_img(screen)
+        #cv2.imshow('window', new_screen)
+        cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+
+        if m1 < 0 and m2 < 0:
+            right()
+        elif m1 > 0  and m2 > 0:
+            left()
+        else:
+            straight()
+
+        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+main()
