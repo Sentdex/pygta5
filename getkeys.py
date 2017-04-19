dedb055e9c110114cacc0498bdc721b0d97b4b27
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-04-19 10:32:59 -0300

    Better way to create the keyList and flake8 fixes

--- getkeys.py
+++ getkeys.py
@@ -1,11 +1,9 @@
 # Citation: Box Of Hats (https://github.com/Box-Of-Hats )
 
 import win32api as wapi
-import time
 
-keyList = ["\b"]
-for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'£$/\\":
-    keyList.append(char)
+keyList = ["\b"] + list("ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'£$/\\")
+
 
 def key_check():
     keys = []
@@ -13,4 +11,3 @@ def key_check():
         if wapi.GetAsyncKeyState(ord(key)):
             keys.append(key)
     return keys
- 
