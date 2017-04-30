6d7a56251cecd236b1cbb5c0f3e5ac9e4f823d64
Colton-Bryce <coltonlbryce@gmail.com>
2017-04-29 22:57:38 -0500

    Removed unnecessary imports

--- grabscreen.py
+++ grabscreen.py
@@ -1,19 +1,21 @@
 # Done by Colton-Bryce
 from PIL import ImageGrab
-import win32gui
+from win32gui import FindWindow, SetForegroundWindow, GetWindowRect
 
-class Camera(object):# Pass the window title e.g. 'Grand Theft Auto V'
-    # Make sure the window is on your main monitor or you will get a black screen.
+class Camera(object):#Pass the window title e.g. 'Grand Theft Auto V'
+    #Make sure the window is on your main monitor or you will get a black screen
 
     def __init__(self, title):
-        self.hwnd = win32gui.FindWindow(None, title)
-        win32gui.SetForegroundWindow(self.hwnd)
+        self.hwnd = FindWindow(None, title)
+        SetForegroundWindow(self.hwnd)
+
 
     def screenshot(self):
         while 1:
-            bbox = win32gui.GetWindowRect(self.hwnd)
-            img = ImageGrab.grab(bbox)
+            self.bbox = GetWindowRect(self.hwnd)
+            img = ImageGrab.grab(self.bbox)
+
             if img.size[0] > 1:
                 break
-                
+
         return img
