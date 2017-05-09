2535835853a6213064e1746a5e8389b0f68f905f
Harrison <Harrison@sentdex.com>
2017-05-09 10:08:54 -0500

    Update collecting-data-for-larger-fpv-model.py
    Changing res to 16:9, no reason for the extra height, also rather than gray, we're going with color. I'd rather have higher quality data collected, and then downsample, rather than change my mind later and have to re-record. I still might change things in time. 
    
    If you already recorded in gray, that's really no big deal, it's still valid training data, I plan to still use my gray.

--- collecting-data-for-larger-fpv-model.py
+++ collecting-data-for-larger-fpv-model.py
@@ -84,8 +84,8 @@ def main():
             # 800x600 windowed mode
             screen = grab_screen(region=(0,40,800,640))
             last_time = time.time()
-            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-            screen = cv2.resize(screen, (160,120))
+            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
+            screen = cv2.resize(screen, (160,90))
             # resize to something a bit more acceptable for a CNN
             keys = key_check()
             output = keys_to_output(keys)
