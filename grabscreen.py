62ff2997c14a382bf2de691aeaa4aabf87aaaef3
Werseter <werseter@interia.eu>
2017-12-29 01:57:42 +0100

    Update grabscreen.py
    Ensured proper object lifespans, preventing unnecessary reinitializations, ensured faster RGBA to RGB conversion, fixed top_left corner region usage, moved to class object for multiple POV and allowed proper cleanup available from outside.

--- grabscreen.py
+++ grabscreen.py
@@ -1,38 +1,98 @@
-# Done by Frannecklp
+"""
+based on work by Frannecklp
+changes by dandrews
+revised and optimized by Werseter
+"""
 
-import cv2
+import win32ui
+import win32gui
+import win32con
 import numpy as np
-import win32gui, win32ui, win32con, win32api
 
-def grab_screen(region=None):
+class grab_screen:
+    def __init__(self, region=None, window_title='Game', window_handle=None):
+        self.region = region
+        self.window_title = window_title
+        self.window_handle = window_handle
+        self.screen = windows_screen_grab(window_title=self.window_title, region=self.region, window_handle=self.window_handle)
 
-    hwin = win32gui.GetDesktopWindow()
 
-    if region:
-            left,top,x2,y2 = region
-            width = x2 - left + 1
-            height = y2 - top + 1
-    else:
-        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
-        height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
-        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
-        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+    def getFrame(self):
+        bits = self.screen.get_screen_bits()
+        rgb = self.screen.get_rgb_from_bits(bits)
+        if self.window_handle is None:
+            self.window_handle = self.screen.getHandle()
+        return rgb
 
-    hwindc = win32gui.GetWindowDC(hwin)
-    srcdc = win32ui.CreateDCFromHandle(hwindc)
-    memdc = srcdc.CreateCompatibleDC()
-    bmp = win32ui.CreateBitmap()
-    bmp.CreateCompatibleBitmap(srcdc, width, height)
-    memdc.SelectObject(bmp)
-    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+    def clear(self):
+        self.screen.cleanup()
+            
+class windows_screen_grab:
+    _hwnd = 0
     
-    signedIntsArray = bmp.GetBitmapBits(True)
-    img = np.fromstring(signedIntsArray, dtype='uint8')
-    img.shape = (height,width,4)
+    def enumHandler(self, hwnd, lParam):
+        if win32gui.IsWindowVisible(hwnd):
+            title = win32gui.GetWindowText(hwnd)            
+            if self._search_str in title:                
+                self._hwnd = hwnd 
+##                print(self._search_str + ' found in hwnd ' + str(hwnd))
+##                print(title)
 
-    srcdc.DeleteDC()
-    memdc.DeleteDC()
-    win32gui.ReleaseDC(hwin, hwindc)
-    win32gui.DeleteObject(bmp.GetHandle())
+    def __init__(self, window_title: str, region = None, window_handle = None):
+        self._search_str = window_title
+        if window_handle:        
+            self._hwnd = window_handle
+        else:
+            win32gui.EnumWindows(self.enumHandler, None)
+            if self._hwnd == 0:
+                message = "window_title '{}' not found.".format(window_title)
+                raise ValueError(message)
+        
+        hwnd = self._hwnd
+             
+        if region is None:
+            rect = win32gui.GetWindowRect(hwnd)
+        else:
+            rect = region
 
-    return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+        t = rect[0]
+        l = rect[1]
+        w = rect[2]
+        h = rect[3]
+        
+        dataBitMap = win32ui.CreateBitmap()        
+        w_handle_DC = win32gui.GetWindowDC(hwnd)
+        windowDC = win32ui.CreateDCFromHandle(w_handle_DC)
+        memDC = windowDC.CreateCompatibleDC()
+        dataBitMap.CreateCompatibleBitmap(windowDC, w, h)
+        memDC.SelectObject(dataBitMap)
+        self._w_handle_DC = w_handle_DC
+        self._dataBitMap = dataBitMap
+        self._memDC = memDC
+        self._windowDC = windowDC
+        self._height = h
+        self._width = w
+        self._top  = t
+        self._left = l
+        self._rgb = np.zeros((3, h, w), dtype=np.int)
+    
+    def get_screen_bits(self):        
+        self._memDC.StretchBlt((0,0), (self._width, self._height), self._windowDC, (self._top, self._left), (self._width, self._height), win32con.SRCCOPY)        
+        bits = np.fromstring(self._dataBitMap.GetBitmapBits(True), np.uint8)
+        bits = np.delete(bits, slice(3, None, 4))
+        return bits
+    
+    def get_rgb_from_bits(self, bits):
+        bits.shape = (self._height, self._width, 3)
+        self._rgb = bits
+        return self._rgb
+    
+    def cleanup(self):     
+        self._windowDC.DeleteDC()
+        self._memDC.DeleteDC()
+        win32gui.ReleaseDC(self._hwnd, self._w_handle_DC)
+        win32gui.DeleteObject(self._dataBitMap.GetHandle())    
+
+    def getHandle(self):
+        return self._hwnd
+   
