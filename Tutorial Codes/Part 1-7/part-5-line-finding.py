6c2555c140c83794fc291809fc4cfbe6423cd971
Zacahry E. Edgell <zacharyedgell@gmail.com>
2019-01-17 05:20:00 -0500

    added a new way to get screen

--- Tutorial Codes/Part 1-7/part-5-line-finding.py
+++ Tutorial Codes/Part 1-7/part-5-line-finding.py
@@ -38,12 +38,16 @@ def process_img(original_image):
 def main():
     last_time = time.time()
     while(True):
-        screen =  np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
+        #find Screen Dimensions#####################
+        hwnd = win32gui.FindWindow("grcWindow", None)
+        dimensions = win32gui.GetWindowRect(hwnd)
+        ############################################
+        screen = np.array(ImageGrab.grab(dimensions))
         new_screen = process_img(screen)
-        print('Loop took {} seconds'.format(time.time()-last_time))
+        print('Loop took {} Seconds'.format(time.time()-last_time))
         last_time = time.time()
         cv2.imshow('window', new_screen)
-        #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
