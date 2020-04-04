c9872e35382123d328d3765176257831e2a1bea3
Eric Picot <eric.picot@hotmail.fr>
2020-04-04 11:47:21 +0200

    cleaning

--- getkeys.py
+++ getkeys.py
@@ -1,20 +1,5 @@
-# # Citation: Box Of Hats (https://github.com/Box-Of-Hats )
-# 
-# import win32api as wapi
-# 
-# keyList = ["\b"]
-# for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'£$/\\":
-#     keyList.append(char)
-# 
-# def key_check():
-#     keys = []
-#     for key in keyList:
-#         if wapi.GetAsyncKeyState(ord(key)):
-#             keys.append(key)
-#     return keys
-# 
 import keyboard as kb
-# while True:
+
 def key_check():
     key_list = []
     key = kb.get_hotkey_name()
